7a83488f9f259b770abc64cb3ce0206160fe0cce
lukasz <lukasz.usarz@gmail.com>
2020-08-05 12:14:42 +0200

    Simplify day formatting

--- app/src/main/java/com/softarea/learningapp/utils/CalendarUtils.java
+++ app/src/main/java/com/softarea/learningapp/utils/CalendarUtils.java
@@ -1,13 +1,6 @@
 package com.softarea.learningapp.utils;
 
 public class CalendarUtils {
-  public static String transformDay(int day) {
-    if( day < 10 ) {
-      return StringUtils.join( "0", String.valueOf(day) );
-    } else {
-      return String.valueOf(day);
-    }
-  }
   public static String transformMonth(int month) {
     String[] months = {"styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"};
     return  months[month - 1];
